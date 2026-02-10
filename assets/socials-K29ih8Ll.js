75e35de93849aafa3c4d707ad28c55eb13c72356
AbdelMeza <hako.mezaguer13@gmail.com>
2026-02-10 13:18:56 +0100

    Updates

---
+++ assets/socials-K29ih8Ll.js
@@ -0,0 +1,1 @@
+const a=[{name:"Github",link:"https://github.com/AbdelMeza"},{name:"Codepen",link:"https://codepen.io/abdelhakcode"},{name:"Instagram",link:"https://instagram.com/abdlllmzz"},{name:"Twitter",link:"https://x.com/abdelhakcode"}];export{a as s};
--- assets/socials-Y49B35CY.js
+++
@@ -1,1 +0,0 @@
-const n=[{name:"Github",link:""},{name:"Codepen",link:""},{name:"Instagram",link:""},{name:"Twitter",link:""}];export{n as s};
